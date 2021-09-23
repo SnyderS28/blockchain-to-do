5a1232c0bf42fcb215bd003f24cf21424ab5f5e2
Steven Snyder <SnyderS28@users.noreply.github.com>
2021-09-23 12:45:46 -0600

    Added task functionality

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,12 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public {
         createTask("This works!");
     }
@@ -18,5 +24,6 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
